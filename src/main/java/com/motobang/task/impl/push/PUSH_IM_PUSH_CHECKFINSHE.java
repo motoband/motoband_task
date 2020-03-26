cbcb5dfc77cb69ccb31a41bf70f9e037983d3d62
401825317 <401825317@qq.com>
2020-03-26 15:25:15 +0800

    1

--- src/main/java/com/motobang/task/impl/push/PUSH_IM_PUSH_CHECKFINSHE.java
+++ src/main/java/com/motobang/task/impl/push/PUSH_IM_PUSH_CHECKFINSHE.java
@@ -10,9 +10,16 @@ import com.github.ltsopensource.core.logger.LoggerFactory;
 import com.github.ltsopensource.tasktracker.Result;
 import com.github.ltsopensource.tasktracker.runner.InterruptibleJobRunner;
 import com.github.ltsopensource.tasktracker.runner.JobContext;
+import com.google.common.collect.Maps;
+import com.motoband.common.Consts;
+import com.motoband.common.MBResponse;
+import com.motoband.common.MBResponseCode;
 import com.motoband.dao.UserDAO;
 import com.motoband.manager.UserManager;
 import com.motoband.model.task.MessageTaskModel;
+import com.motoband.utils.OkHttpClientUtil;
+
+import okhttp3.Headers;
 
 public class PUSH_IM_PUSH_CHECKFINSHE implements InterruptibleJobRunner {
     protected static final Logger LOGGER = LoggerFactory.getLogger(PUSH_IM_PUSH_CHECKFINSHE.class);
@@ -26,32 +33,36 @@ public class PUSH_IM_PUSH_CHECKFINSHE implements InterruptibleJobRunner {
 			return null;
 		}
 		dataMap.put("updatetime", System.currentTimeMillis());
-		LOGGER.error("taskid="+taskid+",开始检查任务是否完成");
 		if (UserManager.getInstance().checkTask(taskid)) {
-			LOGGER.error("taskid="+taskid+",结束检查任务是否完成");
 			dataMap.put("state", 1);
 			if(LOGGER.isErrorEnabled()) {
 				LOGGER.trace("taskid is finshed -------"+taskid+"-----"+JSON.toJSONString(dataMap) );
 			}
-			Job res=jobContext.getJob();
-			res.setRepeatCount(0);
-			res.setReplaceOnExist(true);
-			jobContext.setJob(res);
+			Map<String,Object> map=UserDAO.getLTSTask( taskid);
+			if(map!=null) {
+				String job_id=(String) map.get("job_id");
+				Map<String,String> params=Maps.newHashMap();
+				params.put("jobId", job_id);
+				try {
+					Map<String,String> r=Maps.newHashMap();
+					r.put("Authorization", "Basic bW90b2JhbmQ6TW90b2JhbmQyMDE1IUAjJA==");
+					Headers.of(r);
+					okhttp3.Response response=OkHttpClientUtil.okHttpPost(Consts.LTS_ADMIN_API_IP+"/api/job-queue/repeat-job-delete",params,Headers.of(r));
+					if(response.isSuccessful()){
+					}
+					response.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
 		} else {
-			LOGGER.error("taskid="+taskid+",结束检查任务是否完成");
 			dataMap.put("state", 0);
 		}
 		dataMap.put("taskid", taskid);
-		LOGGER.error("taskid="+taskid+"开始检查任务用户总数");
 		dataMap.put("sumcount", UserManager.getInstance().getUserTaskCount(taskid, -1));
-		LOGGER.error("taskid="+taskid+"结束检查任务用户总数");
-		LOGGER.error("taskid="+taskid+"开始检查任务用户执行成功总数");
 		dataMap.put("successcount", UserManager.getInstance().getUserTaskCount(taskid, 1));
-		LOGGER.error("taskid="+taskid+"开始检查任务用户执行失败总数");
 		dataMap.put("failcount", UserManager.getInstance().getUserTaskCount(taskid, 2));
-		LOGGER.error("taskid="+taskid+"开始更新任务执行情况");
 		UserManager.getInstance().updatetaskmsgliststate(dataMap);
-		LOGGER.error("taskid="+taskid+"结束更新任务执行情况");
 		return null;
 	}
 	
