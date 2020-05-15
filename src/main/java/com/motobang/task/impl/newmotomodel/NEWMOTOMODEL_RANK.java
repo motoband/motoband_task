2258a8ea1a5ad4c0e5e4c4bc1007823b63208dc7
yangjunfei <2572427919@qq.com>
2020-05-15 11:05:06 +0800

    1

--- src/main/java/com/motobang/task/impl/newmotomodel/NEWMOTOMODEL_RANK.java
+++ src/main/java/com/motobang/task/impl/newmotomodel/NEWMOTOMODEL_RANK.java
@@ -4,15 +4,21 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.List;
+import java.util.Map;
 
+import com.alibaba.fastjson.JSON;
 import com.github.ltsopensource.core.logger.Logger;
 import com.github.ltsopensource.core.logger.LoggerFactory;
 import com.github.ltsopensource.tasktracker.Result;
 import com.github.ltsopensource.tasktracker.runner.JobContext;
 import com.github.ltsopensource.tasktracker.runner.JobRunner;
+import com.google.common.collect.Lists;
+import com.google.gson.annotations.JsonAdapter;
 import com.motoband.dao.newmotomodel.NewMotoModelDAO;
 import com.motoband.model.NewMotoModel;
 import com.motoband.model.NewMotoRankModel;
+import com.motoband.utils.MBUtil;
+import com.motoband.utils.MD5;
 
 public class NEWMOTOMODEL_RANK implements JobRunner  {
     protected static final Logger LOGGER = LoggerFactory.getLogger(NEWMOTOMODEL_RANK.class);
@@ -25,9 +31,15 @@ public class NEWMOTOMODEL_RANK implements JobRunner  {
 		long endtime=now.toInstant(ZoneOffset.of("+8")).toEpochMilli();
 		//查询时间段内的线路
 		String sql="select modelid,SUM(mileage) as mileage ,AVG(maxspeed) avgmaxspeed,AVG(avgspeed) avgspeed,count(id) usercount from rideline \r\n" + 
-				"where reporttime>=1585670400000 and reporttime<1585699200000 GROUP BY modelid";
-		List<NewMotoRankModel> res=NewMotoModelDAO.selectList(sql);
-		NewMotoModelDAO.insert(res);
+				"where reporttime>="+starttime+" and reporttime<"+endtime+" GROUP BY modelid";
+		List<Map> res=NewMotoModelDAO.selectList(sql);
+		List<NewMotoRankModel> result=Lists.newArrayList();
+		for (Map newMotoRankModel : res) {
+			newMotoRankModel.put("ranktime", endtime);
+			newMotoRankModel.put("rankid", MD5.stringToMD5(newMotoRankModel.get("modelid")+"-"+endtime));;
+		}
+		result=JSON.parseArray(JSON.toJSONString(res), NewMotoRankModel.class);
+		NewMotoModelDAO.insert(result);
 		return null;
 	}
 }
