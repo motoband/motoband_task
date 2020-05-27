f4f7d5efa746d5efd02641ad0c528cff14ab33b7
yangjunfei <2572427919@qq.com>
2020-05-27 16:23:23 +0800

    1

--- src/main/java/com/motobang/task/impl/newmotomodel/NEWMOTOMODEL_RANK.java
+++ src/main/java/com/motobang/task/impl/newmotomodel/NEWMOTOMODEL_RANK.java
@@ -17,17 +17,11 @@ import com.github.ltsopensource.tasktracker.runner.JobContext;
 import com.github.ltsopensource.tasktracker.runner.JobRunner;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import com.google.gson.annotations.JsonAdapter;
-import com.motoband.common.Consts;
 import com.motoband.dao.newmotomodel.NewMotoModelDAO;
 import com.motoband.manager.MotoDataManager;
-import com.motoband.manager.RedisManager;
 import com.motoband.model.MotoModelModel;
-import com.motoband.model.NewMotoModel;
 import com.motoband.model.NewMotoModelV2;
 import com.motoband.model.NewMotoRankModel;
-import com.motoband.utils.MBUtil;
-import com.motoband.utils.MD5;
 
 public class NEWMOTOMODEL_RANK implements JobRunner  {
     protected static final Logger LOGGER = LoggerFactory.getLogger(NEWMOTOMODEL_RANK.class);
@@ -59,10 +53,10 @@ public class NEWMOTOMODEL_RANK implements JobRunner  {
 		long starttime=now.plusMonths(-1).toInstant(ZoneOffset.of("+8")).toEpochMilli();
 		long endtime=now.toInstant(ZoneOffset.of("+8")).toEpochMilli();
 		//查询时间段内的线路
-		String sql="select brandid,SUM(mileage) as mileage ,AVG(maxspeed) avgmaxspeed,AVG(avgspeed) avgspeed from rideline \r\n" + 
-				"where reporttime>="+starttime+" and reporttime<"+endtime+" GROUP BY brandid";
-//		String sql="select modelid,SUM(mileage) as mileage ,AVG(maxspeed) avgmaxspeed,AVG(avgspeed) avgspeed,count(id) usercount from rideline \r\n" + 
-//"where reporttime>=1585670400000 and reporttime<1585699200000 GROUP BY modelid";
+//		String sql="select brandid,SUM(mileage) as mileage ,AVG(maxspeed) avgmaxspeed,AVG(avgspeed) avgspeed from rideline \r\n" + 
+//				"where reporttime>="+starttime+" and reporttime<"+endtime+" GROUP BY brandid";
+		String sql="select modelid,SUM(mileage) as mileage ,AVG(maxspeed) avgmaxspeed,AVG(avgspeed) avgspeed from rideline \r\n" + 
+"where reporttime>=1585670400000 and reporttime<1585699200000 GROUP BY modelid";
 //		RedisManager.getInstance().hget(Consts.REDIS_SCHEME_NEWS, key, field)
 		List<Map<String,Object>> res=NewMotoModelDAO.selectList(sql);
 		List<NewMotoRankModel> result=Lists.newArrayList();
@@ -163,10 +157,10 @@ public class NEWMOTOMODEL_RANK implements JobRunner  {
 		long starttime=now.plusMonths(-1).toInstant(ZoneOffset.of("+8")).toEpochMilli();
 		long endtime=now.toInstant(ZoneOffset.of("+8")).toEpochMilli();
 		//查询时间段内的线路
-		String sql="select modelid,SUM(mileage) as mileage ,AVG(maxspeed) avgmaxspeed,AVG(avgspeed) avgspeed,count(id) hotcount from rideline \r\n" + 
-				"where reporttime>="+starttime+" and reporttime<"+endtime+" GROUP BY modelid";
-//		String sql="select modelid,SUM(mileage) as mileage ,AVG(maxspeed) avgmaxspeed,AVG(avgspeed) avgspeed,count(id) usercount from rideline \r\n" + 
-//"where reporttime>=1585670400000 and reporttime<1585699200000 GROUP BY modelid";
+//		String sql="select modelid,SUM(mileage) as mileage ,AVG(maxspeed) avgmaxspeed,AVG(avgspeed) avgspeed,count(id) hotcount from rideline \r\n" + 
+//				"where reporttime>="+starttime+" and reporttime<"+endtime+" GROUP BY modelid";
+		String sql="select modelid,SUM(mileage) as mileage ,AVG(maxspeed) avgmaxspeed,AVG(avgspeed) avgspeed from rideline \r\n" + 
+"where reporttime>=1585670400000 and reporttime<1585699200000 GROUP BY modelid";
 		List<Map<String, Object>> res=NewMotoModelDAO.selectList(sql);
 		List<NewMotoRankModel> result=Lists.newArrayList();
 		for (Map<String, Object> newMotoRankModel : res) {
