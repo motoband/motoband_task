d5207086080e06268b35a2ccba02a018c2404438
yangjunfei <2572427919@qq.com>
2020-06-07 16:51:45 +0800

    1

--- src/main/java/com/motobang/task/impl/newmotomodel/NEWMOTOMODEL_RANK.java
+++ src/main/java/com/motobang/task/impl/newmotomodel/NEWMOTOMODEL_RANK.java
@@ -192,7 +192,7 @@ public class NEWMOTOMODEL_RANK implements JobRunner  {
 			newMotoRankModel.put("totalreadcount",totalreadcount);
 			//当月的totalreadcount-上月个totalreadcount
 			long prevranktime= now.plusMonths(-2).toInstant(ZoneOffset.of("+8")).toEpochMilli();
-			sql="select totalreadcount as count from motomodel_new_rank where modelid="+modelid+" and ranktime<="+prevranktime;
+			sql="select totalreadcount as count from motomodel_new_rank where modelid="+modelid+" and ranktime="+prevranktime;
 			long prevtotalreadcount=NewMotoModelDAO.getCountByModelId(sql);
 			long readcount=totalreadcount-prevtotalreadcount;
 			newMotoRankModel.put("readcount",readcount);
@@ -375,7 +375,7 @@ public class NEWMOTOMODEL_RANK implements JobRunner  {
 			newMotoRankModel.put("totalreadcount",totalreadcount);
 			//当月的totalreadcount-上月个totalreadcount
 			long prevranktime= now.plusMonths(-2).toInstant(ZoneOffset.of("+8")).toEpochMilli();
-			sql="select totalreadcount as count from motomodel_new_rank where brandid="+brandid+" and ranktime<="+prevranktime;
+			sql="select totalreadcount as count from motomodel_new_rank where brandid="+brandid+" and ranktime="+prevranktime;
 			long prevtotalreadcount=NewMotoModelDAO.getCountByModelId(sql);
 			long readcount=totalreadcount-prevtotalreadcount;
 			newMotoRankModel.put("readcount",readcount);
