192c21121009eb3dd931ce93bfe820f026c9158c
高宏宇 <910565111@qq.com>
2020-11-28 20:51:41 +0800

    1

--- src/main/java/com/motobang/task/impl/gps/CMQ_WARNING.java
+++ src/main/java/com/motobang/task/impl/gps/CMQ_WARNING.java
@@ -1,6 +1,4 @@
-package com.motoband.task.impl.cmq;
-
-import java.util.List;
+package com.motobang.task.impl.gps;
 
 import com.github.ltsopensource.core.logger.Logger;
 import com.github.ltsopensource.core.logger.LoggerFactory;
@@ -8,14 +6,13 @@ import com.github.ltsopensource.tasktracker.Result;
 import com.github.ltsopensource.tasktracker.runner.JobContext;
 import com.github.ltsopensource.tasktracker.runner.JobRunner;
 import com.motoband.manager.hardware.gps.GPSManager;
-import com.motoband.model.QueueMonitorModel;
 
 public class CMQ_WARNING implements JobRunner{
 	protected static final Logger LOGGER = LoggerFactory.getLogger(CMQ_WARNING.class);
 	@Override
 	public Result run(JobContext arg0) throws Throwable {
 		LOGGER.info("CMQ_WARNING is started");
-		List<QueueMonitorModel> queueMonitorModels= GPSManager.getInstance().CmqVisibleMessageCount();
+		GPSManager.getInstance().CmqVisibleMessageCount();
 		LOGGER.info("CMQ_WARNING is finished");
 		return null;
 	}
