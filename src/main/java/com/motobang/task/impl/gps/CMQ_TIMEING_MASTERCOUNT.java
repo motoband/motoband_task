190018d1ac57cf0b4191e88fa2355f6ec82acb80
401825317 <401825317@qq.com>
2020-12-04 15:01:29 +0800

    1

--- src/main/java/com/motobang/task/impl/gps/CMQ_TIMEING_MASTERCOUNT.java
+++ src/main/java/com/motobang/task/impl/gps/CMQ_TIMEING_MASTERCOUNT.java
@@ -7,6 +7,10 @@ import com.github.ltsopensource.tasktracker.runner.JobContext;
 import com.github.ltsopensource.tasktracker.runner.JobRunner;
 import com.motoband.manager.hardware.gps.GPSManager;
 
+/**
+ * 定时部署
+ * Created by junfei.Yang on 2020年12月4日.
+ */
 public class CMQ_TIMEING_MASTERCOUNT implements JobRunner{
 	protected static final Logger LOGGER = LoggerFactory.getLogger(CMQ_TIMEING_MASTERCOUNT.class);
 	public Result run(JobContext arg0) throws Throwable {
--- src/main/java/com/motobang/task/impl/gps/CMQ_WARNING.java
+++ src/main/java/com/motobang/task/impl/gps/CMQ_WARNING.java
@@ -1,20 +1,33 @@
 package com.motobang.task.impl.gps;
 
+import java.util.ArrayList;
+
+import com.alibaba.fastjson.JSON;
+import com.github.ltsopensource.core.domain.Action;
 import com.github.ltsopensource.core.logger.Logger;
 import com.github.ltsopensource.core.logger.LoggerFactory;
 import com.github.ltsopensource.tasktracker.Result;
+import com.github.ltsopensource.tasktracker.runner.InterruptibleJobRunner;
 import com.github.ltsopensource.tasktracker.runner.JobContext;
-import com.github.ltsopensource.tasktracker.runner.JobRunner;
 import com.motoband.manager.hardware.gps.GPSManager;
+import com.motoband.model.QueueMonitorModel;
 
-public class CMQ_WARNING implements JobRunner{
+/**
+ * cmq消息可见消息数量告警
+ * Created by junfei.Yang on 2020年12月4日.
+ */
+public class CMQ_WARNING implements InterruptibleJobRunner{
 	protected static final Logger LOGGER = LoggerFactory.getLogger(CMQ_WARNING.class);
 	@Override
 	public Result run(JobContext arg0) throws Throwable {
 		LOGGER.info("CMQ_WARNING is started");
-		GPSManager.getInstance().CmqVisibleMessageCount();
+		ArrayList<QueueMonitorModel> res=GPSManager.getInstance().CmqVisibleMessageCount();
 		LOGGER.info("CMQ_WARNING is finished");
-		return null;
+		return new Result(Action.EXECUTE_SUCCESS, JSON.toJSONString(res)) ;
+	}
+	@Override
+	public void interrupt() {
+		LOGGER.info("结束处理...............");
 	}
 
 }
